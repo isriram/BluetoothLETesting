e3f3884ecf999bef5e1ccc6b5282574e69bb9995
Karun Japhet <karun@japhet.in>
2016-05-08 13:37:52 -0400

    Moved from testng to junit+assertj

--- src/test/java/net/sriramiyer/beacon/esurl/BluetoothServiceTester.java
+++ src/test/java/net/sriramiyer/beacon/esurl/BluetoothServiceTester.java
@@ -1,7 +1,9 @@
 package net.sriramiyer.beacon.esurl;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BluetoothServiceTester {
 
@@ -11,43 +13,42 @@ public class BluetoothServiceTester {
     int advertisingInterval = 100;
     BluetoothLEService bluetoothLEService = new BluetoothLEService(signalStrength, advertisingInterval);
     String signal = bluetoothLEService.getSignalStrength();
-    Assert.assertEquals(signal, "ff");
+    assertThat(signal).isEqualTo("ff");
   }
 
-  @Test(expectedExceptions = {IllegalArgumentException.class})
+  @Test(expected = IllegalArgumentException.class)
   public void shouldNotAllowOutOfRangeValuesForSignalStrength() {
-    // values tested and passed : -200, -129, 128, 200
-    // {i.e. range -128 to 127 is valid, and this test fails for that range as expected}
     int signalStrength = 200;
     int advertisingInterval = 100;
-    BluetoothLEService bluetoothLEService = new BluetoothLEService(signalStrength, advertisingInterval);
-    String signal = bluetoothLEService.getSignalStrength();
+
+    new BluetoothLEService(signalStrength, advertisingInterval).getSignalStrength();
   }
 
   @Test
   public void shouldVerifyAdvertisingTimingInterval() {
     BluetoothLEService bluetoothLEService = new BluetoothLEService();
     String advInterval = bluetoothLEService.getAdvertisingInterval();
-    Assert.assertEquals(advInterval, "0x08 0x0006 a000a0000300000000000000000700");
+    assertThat(advInterval).isEqualTo("0x08 0x0006 a000a0000300000000000000000700");
   }
 
   @Test
   public void shouldVerifyAdvertisingTimingIntervalDifferentValue() {
     BluetoothLEService bluetoothLEService = new BluetoothLEService(150);
     String advInterval = bluetoothLEService.getAdvertisingInterval();
-    Assert.assertEquals(advInterval, "0x08 0x0006 f000f0000300000000000000000700");
+
+    assertThat(advInterval).isEqualTo("0x08 0x0006 f000f0000300000000000000000700");
   }
 
   @Test
   public void shouldGetEnableDisableLEAdvertisingParameters() {
     BluetoothLEService bluetoothLEService = new BluetoothLEService();
     bluetoothLEService.enableLEAdvertising();
+
     String leChecker = bluetoothLEService.getLEAdvertising();
-    Assert.assertEquals(leChecker, "0x08 0x000a 01");
+    assertThat(leChecker).isEqualTo("0x08 0x000a 01");
+
     bluetoothLEService.disableLEAdvertising();
     leChecker = bluetoothLEService.getLEAdvertising();
-    Assert.assertEquals(leChecker, "0x08 0x000a 00");
+    assertThat(leChecker).isEqualTo("0x08 0x000a 00");
   }
-
-
 }
--- src/test/java/net/sriramiyer/beacon/esurl/EddystoneTester.java
+++ src/test/java/net/sriramiyer/beacon/esurl/EddystoneTester.java
@@ -1,7 +1,8 @@
 package net.sriramiyer.beacon.esurl;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class EddystoneTester {
 
@@ -9,41 +10,44 @@ public class EddystoneTester {
   public void shouldCheckBasicURL() {
     EddystoneURL eddystoneURL = new EddystoneURL("https://www.google.com/");
     String esurl = eddystoneURL.getMachineReadableURL();
-    Assert.assertEquals(esurl, "10 00 01 67 6f 6f 67 6c 65 00 00 00 00 00 00 00 00 00 00 00");
+    assertThat(esurl).isEqualTo("10 00 01 67 6f 6f 67 6c 65 00 00 00 00 00 00 00 00 00 00 00");
   }
 
   @Test
   public void shouldGetCorrectURLSchemes() {
     EddystoneURL eddystoneURL = new EddystoneURL("http://sriramiyer.net");
     String esurl = eddystoneURL.getMachineReadableURL();
-    Assert.assertEquals(esurl, "10 00 02 73 72 69 72 61 6d 69 79 65 72 0a 00 00 00 00 00 00");
+
+    assertThat(esurl).isEqualTo("10 00 02 73 72 69 72 61 6d 69 79 65 72 0a 00 00 00 00 00 00");
   }
 
   @Test
   public void shouldCheckSpecialCharacters() {
     EddystoneURL eddystoneURL = new EddystoneURL("http://sriramiyer.net/?p=1");
     String esurl = eddystoneURL.getMachineReadableURL();
-    Assert.assertEquals(esurl, "10 00 02 73 72 69 72 61 6d 69 79 65 72 03 3f 70 3d 31 00 00");
+
+    assertThat(esurl).isEqualTo("10 00 02 73 72 69 72 61 6d 69 79 65 72 03 3f 70 3d 31 00 00");
   }
 
   @Test
   public void shouldPadURLToCorrectLength() {
     EddystoneURL eddystoneURL = new EddystoneURL("https://google.com/");
     String esurl = eddystoneURL.getMachineReadableURL();
-    Assert.assertEquals(esurl, "10 00 03 67 6f 6f 67 6c 65 00 00 00 00 00 00 00 00 00 00 00");
+
+    assertThat(esurl).isEqualTo("10 00 03 67 6f 6f 67 6c 65 00 00 00 00 00 00 00 00 00 00 00");
   }
 
   @Test
   public void shouldGetEddystoneURLWithCustomSignalStrength() {
     EddystoneURL eddystoneURL = new EddystoneURL("https://google.com/");
     String esurl = eddystoneURL.getMachineReadableURL(-42);
-    Assert.assertEquals(esurl, "10 ff 03 67 6f 6f 67 6c 65 00 00 00 00 00 00 00 00 00 00 00");
+
+    assertThat(esurl).isEqualTo("10 ff 03 67 6f 6f 67 6c 65 00 00 00 00 00 00 00 00 00 00 00");
   }
 
-  @Test(expectedExceptions = {IllegalArgumentException.class})
+  @Test(expected = IllegalArgumentException.class)
   public void shouldFailMiserablyBecauseLengthIsOverLimit() {
-    EddystoneURL eddystoneURL = new EddystoneURL("https://google.com/1234567890123456789012345678901234567890");
-    String esurl = eddystoneURL.getMachineReadableURL();
+    new EddystoneURL("https://google.com/1234567890123456789012345678901234567890").getMachineReadableURL();
   }
 
 }
