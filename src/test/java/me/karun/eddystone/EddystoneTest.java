2eb394df48a87e85e55acbad50a53e44a435b80d
Karun Japhet <karun@japhet.in>
2016-05-08 16:17:44 -0400

    moving to paramerized tests for easier expansion

--- src/test/java/me/karun/eddystone/EddystoneTest.java
+++ src/test/java/me/karun/eddystone/EddystoneTest.java
@@ -1,73 +1,84 @@
 package me.karun.eddystone;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
 import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Enclosed.class)
 public class EddystoneTest {
-  @Mock
-  private Url url;
-  private Eddystone eddystone;
 
-  @Before
-  public void setup() {
-    eddystone = new Eddystone("00");
+  @RunWith(Parameterized.class)
+  @RequiredArgsConstructor
+  public static class WithValidData {
+    @Mock
+    private Url url;
+    private Eddystone eddystone;
+    private final String protocol;
+    private final String domain;
+    private final String tld;
+    private final String path;
+    private final String expectedOutput;
 
-    when(url.getProtocol()).thenReturn("http://");
-    when(url.getDomain()).thenReturn("google");
-    when(url.getTld()).thenReturn(".com");
-    when(url.getPath()).thenReturn(empty());
+    @Parameters(name = "{0}{1}{2}{3} to {4}")
+    public static List<String[]> data() {
+      return asList(new String[][]{
+        {"http://", "google", ".com", null, "100002676f6f676c650700000000000000000000"},
+        {"http://", "google", ".com/", null, "100002676f6f676c650000000000000000000000"},
+        {"http://", "karun", ".me/", null, "1000026b6172756e2e6d652f0000000000000000"},
+        {"http://", "karun", ".me/", "page", "1000026b6172756e2e6d652f7061676500000000"},
+      });
+    }
+
+    @Before
+    public void setup() {
+      initMocks(this);
+      eddystone = new Eddystone("00");
+
+      when(url.getProtocol()).thenReturn(protocol);
+      when(url.getDomain()).thenReturn(domain);
+      when(url.getTld()).thenReturn(tld);
+      when(url.getPath()).thenReturn(Optional.ofNullable(path));
+    }
+
+    @Test
+    public void shouldConvert() throws UrlTooLongException {
+      assertThat(eddystone.getHexUrl(url)).isEqualTo(expectedOutput);
+    }
   }
 
-  @Test
-  public void shouldProvideHexVersionOfUrl() throws UrlTooLongException {
-    assertThat(eddystone.getHexUrl(url)).isEqualTo("100002676f6f676c650700000000000000000000");
-  }
+  @RunWith(MockitoJUnitRunner.class)
+  public static class WithInvalidData {
+    @Mock
+    private Url url;
 
-  @Test
-  public void shouldHandleSlashForStandardTld() throws UrlTooLongException {
-    when(url.getTld()).thenReturn(".com/");
+    @Before
+    public void setup() {
+      when(url.getProtocol()).thenReturn("http://");
+      when(url.getDomain()).thenReturn("google");
+      when(url.getTld()).thenReturn(".com/");
+      when(url.getPath()).thenReturn(empty());
+    }
 
-    assertThat(eddystone.getHexUrl(url)).isEqualTo("100002676f6f676c650000000000000000000000");
-  }
+    @Test(expected = UrlTooLongException.class)
+    public void shouldNotHexConvertLongUrls() throws UrlTooLongException {
+      when(url.getPath()).thenReturn(Optional.of("extremelyLongTextToMakeTheUrlGoOverTheLimit"));
 
-  @Test
-  public void shouldHexConvertSlashAtEndForNonStandardTld() throws UrlTooLongException {
-    when(url.getDomain()).thenReturn("karun");
-    when(url.getTld()).thenReturn(".me/");
-
-    assertThat(eddystone.getHexUrl(url)).isEqualTo("1000026b6172756e2e6d652f0000000000000000");
-  }
-
-  @Test
-  public void shouldHexConvertNonStandardTld() throws UrlTooLongException {
-    when(url.getDomain()).thenReturn("karun");
-    when(url.getTld()).thenReturn(".me");
-
-    assertThat(eddystone.getHexUrl(url)).isEqualTo("1000026b6172756e2e6d65000000000000000000");
-  }
-
-  @Test
-  public void shouldHexConvertPath() throws UrlTooLongException {
-    when(url.getPath()).thenReturn(of("page"));
-
-    final String output = eddystone.getHexUrl(url);
-
-    assertThat(output).isEqualTo("100002676f6f676c650770616765000000000000");
-  }
-
-  @Test(expected = UrlTooLongException.class)
-  public void shouldNotHexConvertLongUrls() throws UrlTooLongException {
-    when(url.getPath()).thenReturn(of("extremelyLongTextToMakeTheUrlGoOverTheLimit"));
-
-    eddystone.getHexUrl(url);
+      new Eddystone("00").getHexUrl(url);
+    }
   }
 }
